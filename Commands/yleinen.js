5914bad601eacf9451aa587bcc8ab5515478d7ab
Harry Dang <hadang999@gmail.com>
2021-01-19 16:56:37 -0500

    fixed typo, added cursive

--- Commands/yleinen.js
+++ Commands/yleinen.js
@@ -7,7 +7,7 @@ module.exports = {
   guildOnly: true,
   cooldown: 5,
   execute(message, args) {
-    const yleinens = ['yleinen', 'YLEINEN', 'y l e i n e n', '*yleinen*', '**yleinen**', '***yleinen***', '_ylienen_', '~~yleinen~~', '`yleinen`'];
+    const yleinens = ['yleinen', 'YLEINEN', 'y l e i n e n', '*yleinen*', '**yleinen**', '***yleinen***', '_yleinen_', '~~yleinen~~', '`yleinen`', '𝓎𝓁𝑒𝒾𝓃𝑒𝓃'];
     const random = Math.floor(Math.random() * yleinens.length);
     message.channel.send(yleinens[random]);
   },
