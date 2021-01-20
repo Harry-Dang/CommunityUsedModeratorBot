42eed818a4967f0fcafd3f1c5849d6dfae613d9b
Harry Dang <hadang999@gmail.com>
2021-01-19 23:50:48 -0500

    moved function into promise

--- Commands/democracy.js
+++ Commands/democracy.js
@@ -9,42 +9,42 @@ module.exports = {
   guildOnly: true,
   cooldown: 5,
   execute(message, args) {
-    message.guild.members.fetch(message.author).then(member => {
-      if (!(member.roles.cache.some(role => role.name === 'The Tim Beal Council' || role.name === 'Kamala Harris'))) {
+    message.guild.members.fetch(message.author).then(author => {
+      if (!(author.roles.cache.some(role => role.name === 'The Tim Beal Council' || role.name === 'Kamala Harris'))) {
         return message.reply('you don\' have sufficient privileges for this command');
       }
+      const member = message.mentions.members.first();
+      let muteTime = 1;
+      let probability = 0.5;
+      if (args.length >= 2) {
+        muteTime = args[1];
+      }
+      if (args[1]) {
+        muteTime = args[1];
+      }
+      if (args[2] >= 0 && args[2] <= 1) {
+        probability = args[2];
+      }
+      if (member.roles.cache.some(role => role.name === 'Muted') || message.client.muted.has(member)) {
+        return message.reply(`${member} is already muted`);
+      }
+      if (Math.random() < probability) {
+        const roles = member.roles.cache;
+        message.client.muted.set(member, roles);
+        const muteRole = message.guild.roles.cache.find(role => role.name === 'Muted');
+        member.roles.add(muteRole);
+        member.roles.remove(roles);
+        message.reply(`i flipped a coin and got **heads**\n${member} has been muted for ` + muteTime + ' minute(s)');
+        setTimeout(() => {
+          member.roles.remove(muteRole);
+          member.roles.add(roles);
+          message.client.muted.delete(member);
+          message.channel.send('it has been ' + muteTime + ` minute(s) so ${member} has been unmuted`);
+        }, muteTime * msPerMin);
+      }
+      else {
+        return message.reply('i flipped a coin and got **tails**');
+      }
     });
-    const member = message.mentions.members.first();
-    let muteTime = 1;
-    let probability = 0.5;
-    if (args.length >= 2) {
-      muteTime = args[1];
-    }
-    if (args[1]) {
-      muteTime = args[1];
-    }
-    if (args[2] >= 0 && args[2] <= 1) {
-      probability = args[2];
-    }
-    if (member.roles.cache.some(role => role.name === 'Muted') || message.client.muted.has(member)) {
-      return message.reply(`${member} is already muted`);
-    }
-    if (Math.random() < probability) {
-      const roles = member.roles.cache;
-      message.client.muted.set(member, roles);
-      const muteRole = message.guild.roles.cache.find(role => role.name === 'Muted');
-      member.roles.add(muteRole);
-      member.roles.remove(roles);
-      message.reply(`i flipped a coin and got **heads**\n${member} has been muted for ` + muteTime + ' minute(s)');
-      setTimeout(() => {
-        member.roles.remove(muteRole);
-        member.roles.add(roles);
-        message.client.muted.delete(member);
-        message.channel.send('it has been ' + muteTime + ` minute(s) so ${member} has been unmuted`);
-      }, muteTime * msPerMin);
-    }
-    else {
-      return message.reply('i flipped a coin and got **tails**');
-    }
   },
 };
