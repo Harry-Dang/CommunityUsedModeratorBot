1999e2ef15902c3b0da6dde3ab2a481335ac9839
Harry Dang <hadang999@gmail.com>
2021-01-19 22:57:19 -0500

    added probability option

--- Commands/democracy.js
+++ Commands/democracy.js
@@ -2,22 +2,29 @@ const msPerMin = 60000;
 
 module.exports = {
   name: 'democracy',
-  description: 'flips a coin, if heads: mutes the specified member for a minute or `time_length` minutes',
+  description: 'flips a coin, if heads: mutes the specified member for one minute or `time_length` minutes. By default, the coin is fair, but can be changed using `coin_probability`. `coin_probability` must be between 0 and 1',
   aliases: ['mute'],
   args: true,
-  usage: 'user [time_length]',
+  usage: 'user [time_length] [coin_probability]',
   guildOnly: true,
   cooldown: 5,
   execute(message, args) {
     const member = message.mentions.members.first();
     let muteTime = 1;
+    let probability = 0.5;
     if (args.length >= 2) {
       muteTime = args[1];
+    }
+    if (args[1]) {
+      muteTime = args[1];
+    }
+    if (args[2] >= 0 && args[2] <= 1) {
+      probability = args[2];
     }
     if (member.roles.cache.some(role => role.name === 'Muted') || message.client.muted.has(member)) {
       return message.reply(`${member} is already muted`);
     }
-    if (Math.random() >= 0.5) {
+    if (Math.random() >= probability) {
       const roles = member.roles.cache;
       message.client.muted.set(member, roles);
       const muteRole = message.guild.roles.cache.find(role => role.name === 'Muted');
